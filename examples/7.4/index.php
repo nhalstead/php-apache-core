f73c8d24bc1826459b9ac965ec9caf54f7c4bd50
Noah Halstead <nhalstead00@gmail.com>
2021-10-17 15:06:22 -0400

    Updated the Example to include TXT/JSON/PDF/HTML output format

--- examples/7.4/index.php
+++ examples/7.4/index.php
@@ -1,17 +1,61 @@
 <?php
 
-header("Content-Type: text/plain");
+$val = array(
+	"HTTP_X_FORWARDED_BY" => $_SERVER["HTTP_X_FORWARDED_BY"],
+	"REMOTE_ADDR" => $_SERVER["REMOTE_ADDR"],
+	"opcache.enable" => ini_get("opcache.enable"),
+	"opcache.validate_timestamps" => ini_get("opcache.validate_timestamps"),
+	"opcache.max_accelerated_files" => ini_get("opcache.max_accelerated_files"),
+	"opcache.memory_consumption" => ini_get("opcache.memory_consumption"),
+	"opcache.max_wasted_percentage" => ini_get("opcache.max_wasted_percentage"),
+);
 
-echo "HTTP_X_FORWARDED_BY: " . $_SERVER["HTTP_X_FORWARDED_BY"] . PHP_EOL;
-echo "REMOTE_ADDR: " . $_SERVER["REMOTE_ADDR"] . PHP_EOL;
-echo  PHP_EOL;
+switch (strtolower($_GET["format"])) {
+	case "json":
+		header("Content-Type: application/json");
+		echo json_encode($val);
+		exit();
 
-echo "----------------" . PHP_EOL;
-echo "OPCache Options:" . PHP_EOL;
-echo "opcache.enable=" .ini_get("opcache.enable") . PHP_EOL;
-echo "opcache.validate_timestamps=" .ini_get("opcache.validate_timestamps") . PHP_EOL;
-echo "opcache.max_accelerated_files=" .ini_get("opcache.max_accelerated_files") . PHP_EOL;
-echo "opcache.memory_consumption=" .ini_get("opcache.memory_consumption") . PHP_EOL;
-echo "opcache.max_wasted_percentage=" .ini_get("opcache.max_wasted_percentage") . PHP_EOL;
+	case "txt":
+		header("Content-Type: text/plain");
+		foreach ($val as $key => $value) {
+			echo "$key:$value" . PHP_EOL;
+		}
+		exit();
+
+	case "pdf":
+		require_once __DIR__ . '/vendor/autoload.php';
+		$mpdf = new \Mpdf\Mpdf(['mode' => 'c']);
+		$mpdf->SetTitle("PHP Apache Config");
+		$mpdf->SetSubject("PHP Apache Core Docker Image Config");
+
+		$html = "<table>";
+		$html .= "<thead>";
+		$html .= "<tr><th>Key</th><th>Value</th></tr>";
+		$html .= "</thead>";
+		$html .= "<tbody>";
+		foreach ($val as $key => $value) {
+			$html .= "<tr><td>$key</td><td>$value</td></tr>";
+		}
+		$html .= "</tbody>";
+		$html .= "</table>";
+		$mpdf->WriteHTML($html);
+		$mpdf->Output();
+
+	case "html":
+	default:
+		header("Content-Type: text/html");
+		echo "<table>";
+		echo "<thead>";
+		echo "<tr><th>Key</th><th>Value</th></tr>";
+		echo "</thead>";
+		echo "<tbody>";
+		foreach ($val as $key => $value) {
+			echo "<tr><td>$key</td><td>$value</td></tr>";
+		}
+		echo "</tbody>";
+		echo "</table>";
+		exit();
+}
 
 ?>
